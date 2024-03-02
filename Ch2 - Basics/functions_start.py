fc7a24b946fdc190153d85db9c77df8260bc2e80
Shamsu Baba-Alwaiz <shamsubaba@gmail.com>
2024-03-02 14:14:19 -0500

    changes

--- Ch2 - Basics/functions_start.py
+++ Ch2 - Basics/functions_start.py
@@ -4,18 +4,44 @@
 #
 
 
-# TODO: define a basic function
-
+# TODO: define a basic function["""This is an example of a simple function that takes in one argument and returns the square of that value."""]
+def func1():
+    print("I am a function")
 
 # TODO: function that takes arguments
-
+def func2(arg1, arg2):
+    print(arg1, " ", arg2)
 
 # TODO: function that returns a value
-
+def cube(x):
+    return x * x * x
 
 # TODO: function with default value for an argument
+def power(num, x=1):
+    result = 1;
+    for i in range(x):
+        result = result * num
+        return result
+
 
 
 # TODO: function with variable number of arguments
+def multi_add(*args):
+    result = 0
+    for x in args:
+        result += x
+    return result
+# func1()
+# print(func1())
+# print(func1) 
 
+# func2(10, 20)
+# print(func2(10,20))
+# print(cube(3))
 
+# print(power(2))
+# print(power(2,3))
+
+# print(power(x=3, num=2))
+
+print(multi_add(4,5,10,4,10))
