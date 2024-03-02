9d69de9dbb2d3b9fea454b494e9cceafc3533a3f
Shamsu Baba-Alwaiz <shamsubaba@gmail.com>
2024-03-02 11:35:43 -0500

    changes

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,12 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    
+    print(f"Hello, World!")  # print
+    name  = input("What is your name? ") 
+    print("Nice to meet you", name)   # string concatenation
 
+if __name__ == "__main__":
+    main()
+    
